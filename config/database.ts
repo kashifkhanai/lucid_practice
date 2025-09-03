5a4d16127e86981f00d61d382fc70d6801a67e89
kashifkhanai <mkashifkhanai@gmail.com>
2025-09-03 16:18:25 +0500

    Added read-write replicas config (ideal example)

--- config/database.ts
+++ config/database.ts
@@ -13,6 +13,16 @@ const dbConfig = defineConfig({
         password: env.get('DB_PASSWORD'),
         database: env.get('DB_DATABASE'),
       },
+      // // it is possible to configure read and write connections to handle traffic split and use when we have multiple DB servers
+      // // it improves performance and reliability
+      // replicas: {
+      //   read: {
+      //     connection: [{ host: env.get('DB_HOST1_READ') }, { host: env.get('DB_HOST2_READ') }],
+      //   },
+      //   write: {
+      //     connection: { host: env.get('DB_HOST_WRITE') },
+      //   },
+      // },
       migrations: {
         naturalSort: true,
         paths: ['database/migrations'],
