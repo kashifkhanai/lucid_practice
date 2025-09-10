dabbac90517af797f1a9f4817656e0504ed45a31
kashifkhanai <mkashifkhanai@gmail.com>
2025-09-10 18:19:51 +0500

    Create new table form column of existing column

--- app/models/employee.ts
+++ app/models/employee.ts
@@ -1,6 +1,8 @@
 //Creating a new model for testing  .defert()/Separate & Shared Migrations Concept
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
+import EmployeeEmail from '#models/employee_email'
 
 export default class Employee extends BaseModel {
   @column({ isPrimary: true })
@@ -9,8 +11,14 @@ export default class Employee extends BaseModel {
   @column()
   declare name: string
 
-  @column()
-  declare email: string
+  // // remove that column if you are moving email to separate table
+  //..............................................................
+  // @column()
+  // declare email: string
+  //..............................................................
+
+  @hasMany(() => EmployeeEmail)
+  declare emails: HasMany<typeof EmployeeEmail>
 
   @column()
   declare department: string | null
---
+++ database/migrations/1757509025702_create_move_email_to_employee_emails_table.ts
@@ -0,0 +1,46 @@
+// Creating new table for extracting email from employees table in a separate table
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'employee_emails'
+
+  async up() {
+    // create employee_emails table
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('employee_id').unsigned().references('employees.id').onDelete('CASCADE')
+      table.string('email')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+
+    //copy email data from employees table to employee_emails table
+    this.defer(async (db) => {
+      const employees = await db.from('employees').select('*')
+      await Promise.all(
+        employees.map((emp) => {
+          if (emp.email) {
+            return db.table(this.tableName).insert({
+              employee_id: emp.id,
+              email: emp.email,
+            })
+          }
+          return Promise.resolve()
+        })
+      )
+    })
+
+    // delete email column from employees table
+    this.schema.alterTable('employees', (table) => {
+      table.dropColumn('email')
+    })
+  }
+
+  public async down() {
+    // for rollback, add email column back to employees table
+    this.schema.alterTable(this.tableName, (table) => {
+      table.string('email')
+    })
+    this.schema.dropTable(this.tableName)
+  }
+}
